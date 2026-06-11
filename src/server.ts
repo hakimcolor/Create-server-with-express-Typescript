377a192fc6662f22ec457930f3f4b4f1921991ba
hakimcolor <hakimcolor777@gmail.com>
2026-06-11 11:24:24 +0600

    get method

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
-import express from "express"
-const app = express();
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from 'express';
+const app: Application = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/', (req: Request, res: Response) => {
+  // res.send('Hello World!');
+  res.status(200).json({
+    message: 'express server is runign ',
+    'statuse code': 200,
+  });
 });
+// app.post ('/post')
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
