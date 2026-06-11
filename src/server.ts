390f300786232b61daaa1aed4c7cbfa3df638ef7
hakimcolor <hakimcolor777@gmail.com>
2026-06-11 11:37:14 +0600

    the express request and response for post

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import express, {
 } from 'express';
 const app: Application = express();
 const port = 3000;
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   // res.send('Hello World!');
@@ -13,7 +14,13 @@ app.get('/', (req: Request, res: Response) => {
     'statuse code': 200,
   });
 });
-// app.post ('/post')
+app.post('/post', async (req: Request, res: Response) => {
+  // Handle POST request
+  console.log(req.body);
+  res.status(200).json({
+    stats: 'done',
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
