d1db3caf54d0470d2309dc44969a42814931964e
hakimcolor <hakimcolor777@gmail.com>
2026-06-11 21:23:26 +0600

    add neon db and pool

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,16 @@ import express, {
   type Request,
   type Response,
 } from 'express';
-import { log } from 'node:console';
+
+import { Pool } from 'pg';
 const app: Application = express();
 const port = 3000;
 app.use(express.json());
 app.use(express.text());
+const poll = new Pool({
+  connectionString:
+    'postgresql://neondb_owner:npg_5sfmlBF0Xyju@ep-lingering-bonus-atrqv1t1-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+});
 
 app.get('/', (req: Request, res: Response) => {
   // res.send('Hello World!');
@@ -21,7 +26,7 @@ app.post('/post', async (req: Request, res: Response) => {
   console.log(req.body);
   const { name, roll, sex, position } = req.body;
   const data = { name, roll, sex };
-  console.log('the data is this ',data);
+  console.log('the data is this ', data);
   res.status(200).json({
     message: 'request done',
     data: data,
