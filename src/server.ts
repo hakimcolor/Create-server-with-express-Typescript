46d2a54c284a6661c01cfc4c4bf488d659dfc073
hakimcolor <hakimcolor777@gmail.com>
2026-06-12 13:00:53 +0600

    for url use env and config index ts and save good

--- src/server.ts
+++ src/server.ts
@@ -63,16 +63,16 @@ import express, {
 } from 'express';
 
 import { Pool } from 'pg';
+import config from './config';
 
 const app: Application = express();
-const port = 3000;
+const port = config.port;
 
 app.use(express.json());
 app.use(express.text());
 
 const pool = new Pool({
-  connectionString:
-    'postgresql://neondb_owner:npg_5sfmlBF0Xyju@ep-lingering-bonus-atrqv1t1-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=verify-full&channel_binding=require',
+  connectionString: config.connection_string,
 });
 
 const initDB = async () => {
