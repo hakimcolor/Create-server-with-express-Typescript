3ebbe03a74e90cbc7f9de4860e843a5a90233e26
hakimcolor <hakimcolor777@gmail.com>
2026-06-13 10:27:09 +0600

    user controller for the function

--- src/modules/users/user.router.ts
+++ src/modules/users/user.router.ts
@@ -1,25 +1,10 @@
 import { Router, type Request, type Response } from 'express';
 
 import { pool } from '../../db';
+import createUser from './user.contoroller';
 
 const router = Router();
 
-router.get('/', async (req: Request, res: Response) => {
-  try {
-    const result = await pool.query(`
-        SELECT * FROM "user"`);
-    res.status(200).json({
-      success: true,
-      message: 'users retrived succesfully',
-      data: result.rows,
-    });
-  } catch (error: any) {
-    res.status(500).json({
-      success: false,
-      message: error.message,
-      error: error,
-    });
-  }
-});
+router.get('/', createUser);
 
 export const userRoute = router;
