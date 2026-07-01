f57e9ff231d00121eaf24d58811dd159972b0d1b
hakimcolor <hakimcolor777@gmail.com>
2026-07-01 19:57:20 +0600

    password number

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -5,7 +5,7 @@ import { stringify } from 'node:querystring';
 import config from '../../config';
 const loginguserintodb = async (ppayload: {
   email: string;
-  password: string;
+  password: number
 }) => {
   const { email, password } = ppayload;
   const userdata = await pool.query(
